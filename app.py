fd69cf0f9fc27764c7512b1a9c9e398fc1f8f0ec
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-08 11:25:04 +0200

    TP1_1

--- app.py
+++ app.py
@@ -1,54 +1,47 @@
 from flask import Flask
-import flask
-from database.database import db, init_database
-import database.models
-from sar2019.config import Config
-from sar2019.forms import PostEditForm
+from flask import render_template
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
-
-with app.test_request_context():
-    init_database()
 
 
-@app.route('/')
-def index():
-    posts = database.models.Post.query.all()
-    return flask.render_template("homepage.html.jinja2",
-                                 posts=posts)
+@app.route('/greetings')
+def some_function():
+    return "Helloworld!"
 
 
-@app.route("/posts/edit/", methods=["GET", "POST"])
-@app.route("/posts/edit/<post_id>", methods=["GET", "POST"])
-def create_or_process_post(post_id=None):
-
-    # Fetch the corresponding post from the database. If no ID is provided,
-    # then post will be 'None', and the form will consider this value
-    # as a sign that a new post should be created
-    post = database.models.Post.query.filter_by(id=post_id).first()
-    form = PostEditForm(obj=post)
-
-    if form.validate_on_submit():
-        if post is None:
-            post = database.models.Post()
-            post.user_id = 1
-        post.title = form.title.data
-        post.content = form.content.data
-        db.session.add(post)
-        db.session.commit()
-
-        return flask.redirect(flask.url_for('index'))
-    return flask.render_template('edit_post_form.html.jinja2', form=form, post=post)
+@app.route('/greetings_with_html')
+def some_function_html():
+    return """
+<html>
+    <head>
+        <title>Hello!</title>
+    </head>
+    <body>
+        <b>Hello world!</b>
+    </body>
+</html>
+    """
 
 
-@app.route("/posts/delete/<post_id>")
-def delete_post(post_id=None):
-    post = database.models.Post.query.filter_by(id=post_id).first()
-    db.session.delete(post)
-    db.session.commit()
-    return flask.redirect(flask.url_for('index'))
+@app.route('/greetings_with_html_and_template')
+def some_function_html_with_template():
+
+    colors = ["red", "blue", "green", "purple", "dark", "white"]
+    boolean_value = False
+    msg_if_boolean_value = "[boolean_value is true]"
+    msg_if_not_boolean_value = "[boolean_value is false]"
+
+    return render_template("first_template.html",
+                           colors_arg=colors,
+                           boolean_value_arg=boolean_value,
+                           msg_if_boolean_value_arg=msg_if_boolean_value,
+                           msg_if_not_boolean_value_arg=msg_if_not_boolean_value)
+
+
+@app.route("/sum/<label>/<int:a>/<int:b>")
+def compute_sum(label, a, b):
+    c = a + b
+    return label+" "+str(a)+" et "+str(b)+" est "+str(c)
 
 
 if __name__ == '__main__':
--- sar2019/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email
-
-
-class PostEditForm(Form):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
