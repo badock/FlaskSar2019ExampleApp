430b7997e4467cb66a66ddb74271d5b4793a138f
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-14 15:04:44 +0200

    correction

--- app.py
+++ app.py
@@ -1,54 +1,33 @@
-from flask import Flask
 import flask
-from database.database import db, init_database
-import database.models
 from sar2019.config import Config
-from sar2019.forms import PostEditForm
+from sar2019.forms import AdditionForm
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config.from_object(Config)
-db.init_app(app)
-
-with app.test_request_context():
-    init_database()
 
 
-@app.route('/')
-def index():
-    posts = database.models.Post.query.all()
-    return flask.render_template("homepage.html.jinja2",
-                                 posts=posts)
+def traitement_formulaire_addition(form):
+    expression = "%s %s %s" % (form.number_a.data,
+                               form.operator.data,
+                               form.number_b.data)
+    resultat = "%s" % eval(expression)
+    return resultat
 
 
-@app.route("/posts/edit/", methods=["GET", "POST"])
-@app.route("/posts/edit/<post_id>", methods=["GET", "POST"])
-def create_or_process_post(post_id=None):
+def afficher_formulaire_addition(form):
+    return flask.render_template("form_addition.html.jinja2", form=form)
 
-    # Fetch the corresponding post from the database. If no ID is provided,
-    # then post will be 'None', and the form will consider this value
-    # as a sign that a new post should be created
-    post = database.models.Post.query.filter_by(id=post_id).first()
-    form = PostEditForm(obj=post)
+
+@app.route("/add", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
+def fonction_formulaire_addition():
+
+    form = AdditionForm()
 
     if form.validate_on_submit():
-        if post is None:
-            post = database.models.Post()
-            post.user_id = 1
-        post.title = form.title.data
-        post.content = form.content.data
-        db.session.add(post)
-        db.session.commit()
-
-        return flask.redirect(flask.url_for('index'))
-    return flask.render_template('edit_post_form.html.jinja2', form=form, post=post)
-
-
-@app.route("/posts/delete/<post_id>")
-def delete_post(post_id=None):
-    post = database.models.Post.query.filter_by(id=post_id).first()
-    db.session.delete(post)
-    db.session.commit()
-    return flask.redirect(flask.url_for('index'))
+        return traitement_formulaire_addition(form)
+    else:
+        return afficher_formulaire_addition(form)
 
 
 if __name__ == '__main__':
--- sar2019/forms.py
+++ sar2019/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email
+
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
 
 
-class PostEditForm(Form):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+def operator_check(form, field):
+    authorized_operator = ["+", "-", "*", "/", "%"]
+    if field.data not in authorized_operator:
+        raise ValidationError('Field must be in %s' % authorized_operator)
+
+
+class AdditionForm(Form):
+    number_a = IntegerField('Number A', validators=[DataRequired()])
+    number_b = IntegerField('Number B', validators=[DataRequired()])
+    operator = StringField('Operator', validators=[DataRequired(), operator_check])
