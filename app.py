e49cdf4cdc214055fe4e63fd6b804c298d1b4a52
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-16 10:53:00 +0200

    remove correction

--- app.py
+++ app.py
@@ -33,44 +33,9 @@ def index():
                           mimetype="text")
 
 
-@app.route('/engineer/no_templates/<int:engineer_id>')
-def show_engineer_without_templates(engineer_id):
-    engineer = get_engineer_by_id(engineer_id)
-
-    result = """<p>Information about \""""
-    result += engineer.username
-    result += """\"</p>
-
-<dl>
-    <dt>id</dt>
-    <dd>"""
-    result += str(engineer.id)
-    result += """</dd>
-
-    <dt>username</dt>
-    <dd>"""
-    result += engineer.username
-    result += """</dd>
-
-    <dt>email</dt>
-    <dd>"""
-    result += engineer.email
-    result += """"</dd>
-
-    <dt>site</dt>
-    <dd>"""
-    result += engineer.site
-    result += """"</dd>
-</dl>"""
-
-    return result
-
-
 @app.route('/engineer/<int:engineer_id>')
 def show_engineer(engineer_id):
-    engineer = get_engineer_by_id(engineer_id)
-    return flask.render_template("show_engineer.html.jinja2",
-                                 engineer=engineer)
+    return "TODO: codez moi!"
 
 
 if __name__ == '__main__':
