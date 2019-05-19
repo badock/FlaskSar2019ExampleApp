dca00ad9ff1ae1a6f8a76ca43f58def465f11a02
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-19 11:09:37 +0200

    TP templates avancees complet

--- app.py
+++ app.py
@@ -4,47 +4,19 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/greetings')
-def some_function():
-    return "Helloworld!"
+@app.route('/view/a')
+def view_a():
+    return render_template("template_a.html.jinja2")
 
 
-@app.route('/greetings_with_html')
-def some_function_html():
-    return """
-<html>
-    <head>
-        <title>Hello!</title>
-    </head>
-    <body>
-        <b>Hello world!</b>
-    </body>
-</html>
-    """
+@app.route('/view/b')
+def view_b():
+    return render_template("template_b.html.jinja2")
 
 
-@app.route('/greetings_with_html_and_template')
-def some_function_html_with_template():
-
-    colors = ["red", "blue", "green", "purple", "dark", "white"]
-    boolean_value = False
-    msg_if_boolean_value = "[boolean_value is true]"
-    msg_if_not_boolean_value = "[boolean_value is false]"
-
-    return render_template("first_template.html",
-                           colors_arg=colors,
-                           boolean_value_arg=boolean_value,
-                           msg_if_boolean_value_arg=msg_if_boolean_value,
-                           msg_if_not_boolean_value_arg=msg_if_not_boolean_value)
-
-
-@app.route("/sum/<lang>/<int:a>/<int:b>")
-def compute_sum(lang, a, b):
-    c = a + b
-    if lang == "fr":
-        return "La somme de %d et %d est %d" % (a, b, c)
-    else:
-        return "The sum of %d and %d is %d" % (a, b, c)
+@app.route('/view/c')
+def view_c():
+    return render_template("template_c.html.jinja2")
 
 
 if __name__ == '__main__':
