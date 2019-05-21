ad9f03fbe9c981551b4693a7493ea3e43f1031c3
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-21 12:27:19 +0200

    starter for DB session

--- app.py
+++ app.py
@@ -4,24 +4,11 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/view/a')
-def view_a():
-    return render_template("template_a.html.jinja2")
-
-
-@app.route('/view/b')
-def view_b():
-    return render_template("template_b.html.jinja2")
-
-
-@app.route('/view/c')
-def view_c():
-    return render_template("template_c.html.jinja2")
-
-
-@app.route('/static_resources_view')
-def static_resources_view():
-    return render_template("static_resources.html.jinja2")
+@app.route('/engineers')
+def display_engineer():
+    engineers = []
+    return render_template("show_engineers.html.jinja2",
+                           engineers=engineers)
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++
@@ -1,4 +0,0 @@
-(function update_caption() {
-    let caption_text = "logo IMT Atlantique";
-    document.getElementById("caption_logo").innerText = caption_text;
-})();
