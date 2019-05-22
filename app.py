09bf230de28159673b9d1fd62b43e6d4ed1595a3
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-22 02:54:26 +0200

    save work

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import flask
 from database.database import db, init_database
 
-from database.models import Task
+from database.models import Task, TaskList
 from misc.forms import NewTaskForm
 
 app = Flask(__name__)
@@ -16,48 +16,62 @@ with app.test_request_context():
     init_database()
 
 
-def process_new_task(form):
-    print("Adding task '%s'" % form.label.data)
+def process_new_task(form, task_list):
+    print("Adding task '%s' to '%s'" % (form.label.data, task_list.name))
     new_task = Task(label=form.label.data,
-                    isDone=False)
+                    isDone=False,
+                    task_list_id=task_list.id)
     db.session.add(new_task)
     db.session.commit()
 
 
+@app.route("/<tasks_list_name>", methods=["GET", "POST"])
 @app.route("/", methods=["GET", "POST"])
-def homepage():
+def homepage(tasks_list_name="default"):
     form = NewTaskForm()
 
-    if form.validate_on_submit():
-        process_new_task(form)
-        return flask.redirect(flask.url_for("homepage"))
+    tasks_list = TaskList.query.filter_by(name=tasks_list_name).first()
 
-    tasks = Task.query.all()
+    if tasks_list is None:
+        tasks_list = TaskList(name=tasks_list_name)
+        db.session.add(tasks_list)
+        db.session.commit()
+
+    if form.validate_on_submit():
+        process_new_task(form, tasks_list)
+        return flask.redirect(flask.url_for("homepage",
+                                            tasks_list_name=tasks_list.name))
+
     return flask.render_template("show_tasks.html.jinja2",
-                                 tasks=tasks,
+                                 tasks_list=tasks_list,
                                  form=form)
 
 
 @app.route("/toggle_task/<int:task_id>")
 def toggle_task(task_id):
     existing_task = Task.query.filter_by(id=task_id).first()
+    tasks_list = existing_task.tasks_list
+
     existing_task.isDone = not existing_task.isDone
 
     db.session.add(existing_task)
     db.session.commit()
 
-    return flask.redirect(flask.url_for("homepage"))
+    return flask.redirect(flask.url_for("homepage",
+                                        tasks_list_name=tasks_list.name))
 
 
 @app.route("/delete_task/<int:task_id>")
 def delete_task(task_id):
     existing_task = Task.query.filter_by(id=task_id).first()
-    existing_task.isDone = not existing_task.isDone
+
+    tasks_list = existing_task.tasks_list
 
     db.session.delete(existing_task)
     db.session.commit()
 
-    return flask.redirect(flask.url_for("homepage"))
+    return flask.redirect(flask.url_for("homepage",
+                                        tasks_list_name=tasks_list.name))
 
 
 if __name__ == '__main__':
--- database/models.py
+++ database/models.py
@@ -1,7 +1,15 @@
 from database.database import db
 
 
+class TaskList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    tasks = db.relationship('Task', backref='tasks_list', lazy='dynamic')
+
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     label = db.Column(db.Text)
     isDone = db.Column(db.Boolean)
+    task_list_id = db.Column(db.Integer, db.ForeignKey('task_list.id'))
+
