fe3b242c52f27ddfb52b1623fd30bda8b854ea85
Jonathan Pastor <jonathancmoa@gmail.com>
2019-05-23 14:15:35 +0200

    remove parts that students have to code

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
 from flask import Flask
 import flask
-from database.database import db, init_database
 
-from database.models import Task, TaskList
 from misc.forms import NewTaskForm
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secret_key1234"
-
-db.init_app(app)
-
-with app.test_request_context():
-    init_database()
 
 
 def save_object_to_db(db_object):
-    db.session.add(db_object)
-    db.session.commit()
+    # TODO: implement me
+    return None
 
 
 def remove_object_from_db(db_object):
-    db.session.delete(db_object)
-    db.session.commit()
+    # TODO: implement me
+    return None
 
 
 def add_task_to_tasks_list(form, tasks_list):
@@ -59,7 +50,7 @@ def homepage(tasks_list_name="default"):
     if form.validate_on_submit():
         add_task_to_tasks_list(form, tasks_list)
         return flask.redirect(flask.url_for("homepage",
-                                            tasks_list_name=tasks_list.name))
+                                            tasks_list_name=tasks_list_name))
 
     return flask.render_template("show_tasks.html.jinja2",
                                  tasks_list=tasks_list,
@@ -73,7 +64,7 @@ def toggle_task(task_id):
 
     existing_task.isDone = not existing_task.isDone
 
-    save_object_to_db(existing_task)
+    #save_object_to_db(existing_task)
 
     return flask.redirect(flask.url_for("homepage",
                                         tasks_list_name=tasks_list.name))
@@ -85,7 +76,7 @@ def delete_task(task_id):
 
     tasks_list = existing_task.tasks_list
 
-    remove_object_from_db(existing_task)
+    #remove_object_from_db(existing_task)
 
     return flask.redirect(flask.url_for("homepage",
                                         tasks_list_name=tasks_list.name))
--- database/database.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-def init_database():
-    db.create_all()
