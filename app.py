b64f2f8d52407519c1434803a6152d007e73d19e
Jonathan Pastor <jonathancmoa@gmail.com>
2019-06-07 13:45:45 +0200

    first draft for a project template

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import flask
 from database.database import db, init_database
-import database.models
+from database.functions import get_all_engineers, get_engineer_by_id
 from sar2019.config import Config
+from database.models import Mission
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,62 +13,19 @@ with app.test_request_context():
     init_database()
 
 
-def get_all_engineers():
-    return database.models.Engineer.query.all()
-
-
-def get_engineer_by_id(engineer_id):
-    return database.models.Engineer.query.filter_by(id=engineer_id).first()
-
-
-def get_engineers_in_site(site_name):
-    return database.models.Engineer.query.filter_by(site=site_name).all()
-
-
 @app.route('/')
+@app.route('/engineers')
 def index():
     engineers = get_all_engineers()
-
-    result = "All engineers:\n"
-
-    for engineer in engineers:
-        result += " - %s (id:%s)\n" % (engineer.username, engineer.id)
-
-    return flask.Response(result,
-                          mimetype="text")
+    return flask.render_template("list_engineers.html.jinja2",
+                                 engineers=engineers)
 
 
-@app.route('/engineer/no_templates/<int:engineer_id>')
-def show_engineer_without_templates(engineer_id):
-    engineer = get_engineer_by_id(engineer_id)
-
-    result = """<p>Information about \""""
-    result += engineer.username
-    result += """\"</p>
-
-<dl>
-    <dt>id</dt>
-    <dd>"""
-    result += str(engineer.id)
-    result += """</dd>
-
-    <dt>username</dt>
-    <dd>"""
-    result += engineer.username
-    result += """</dd>
-
-    <dt>email</dt>
-    <dd>"""
-    result += engineer.email
-    result += """"</dd>
-
-    <dt>site</dt>
-    <dd>"""
-    result += engineer.site
-    result += """"</dd>
-</dl>"""
-
-    return result
+@app.route('/missions')
+def missions():
+    missions = Mission.query.all()
+    return flask.render_template("list_missions.html.jinja2",
+                                 missions=missions)
 
 
 @app.route('/engineer/id/<int:engineer_id>')
